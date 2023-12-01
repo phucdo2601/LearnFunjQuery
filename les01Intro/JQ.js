c5c01c48c01f183e99ed6f4dd14ffa2310ca81c2
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-12-01 11:18:23 +0700

    [learn-fun-jquery-b01] 01. Basic intro and first learn jquery selector b01

---
+++ les01Intro/JQ.js
@@ -0,0 +1,19 @@
+const func1 = () => {
+    // $(element name)
+    // document.getElementsByTagName(ele name)
+
+    // $("div").css("background-color", "orange");
+
+    // $("#p1").css("font-style", "italic");
+
+    // $(".para").css("font-style", "italic");
+
+    // $("#div1,#div3").css("background-color", 'orange');
+
+    // $('#div1, li').css("background-color", "purple")
+
+    // $("div  p").fadeToggle();
+    // $("p:first").fadeToggle();
+
+    $("li:odd").fadeToggle();
+}
